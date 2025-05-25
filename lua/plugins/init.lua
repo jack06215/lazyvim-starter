881865a75e99fef0de33d5ac6ebfa371e4c61723
Jack Cho <jack06215@gmail.com>
2025-05-25 18:14:53 +0900

    add markdown plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,14 @@
 -- Return a flat list of all plugin specs from separate files
 return vim.tbl_flatten({
+  
   require("plugins.colors"),
   require("plugins.extras"),
+  require("plugins.fzf"),
   require("plugins.lsp_python"),
   require("plugins.lsp_typescript"),
   require("plugins.mason"),
   require("plugins.telescope"),
   require("plugins.todo_comments"), 
   require("plugins.treesitter"),
-  require("plugins.fzf"),
   require("plugins.ui"),
 })
