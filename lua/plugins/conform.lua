579da8c37b437bce93fe50f8a9e5c0a658fe63a8
Jack Cho <jack06215@gmail.com>
2025-06-15 11:47:20 +0900

    add formatter auto run on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,7 +5,15 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         python = { "isort", "black", "mypy" },
+        typescriptreact = { "prettier" },
+        typescript = { "prettier" },
       },
+    }),
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function(args)
+        -- Wiat for 3 seconds.
+        require("conform").format({ bufnr = args.buf, timeout_ms = 5000 })
+      end,
     })
   },
 }
