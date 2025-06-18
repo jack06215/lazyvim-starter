98bdf68f43115dfb8cfc3e107f008da42eeceb4f
Jack Cho <jack06215@gmail.com>
2025-06-18 22:42:41 +0900

    add hardtime

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    enabled = true,
+    lazy = false,
+    opts = {
+      -- disable_mouse = false,
+      -- allow_different_key = false,
+      disabled_filetypes = { "qf", "NvimTree", "lazy", "mason", "neo-tree", "noice", "NeogitStatus" },
+      -- disabled_keys = {},
+    },
+  },
+}
