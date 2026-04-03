b7810ed6410ccecb2a34534638fe28def46d59da
Jack Cho <jack06215@gmail.com>
2026-04-04 00:23:03 +0900

    fix which key

--- lua/plugins/which-key/config.lua
+++ lua/plugins/which-key/config.lua
@@ -2,15 +2,6 @@ return {
   "folke/which-key.nvim",
   opts = {
     spec = {
-      -- File search
-      {
-        "<leader><space>",
-        function()
-          require("telescope.builtin").find_files()
-        end,
-        desc = "Find Files",
-      },
-
       -- Buffer
       { "<leader>b", group = "Buffer" },
 
