a71ed4b0ce83e960eda36ba7d9cd3d5ba8eda225
Jack Cho <jack06215@gmail.com>
2026-04-18 12:34:59 +0900

    add window size adjustment

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>w>", function()
+  local win = vim.api.nvim_get_current_win()
+  local width = vim.api.nvim_win_get_width(win)
+  vim.api.nvim_win_set_width(win, width + 20)
+end, { desc = "Increase window width" })
+
+vim.keymap.set("n", "<leader>w+", function()
+  local win = vim.api.nvim_get_current_win()
+  local height = vim.api.nvim_win_get_height(win)
+  vim.api.nvim_win_set_height(win, height + 10)
+end, { desc = "Increase window width" })
+
 require("config.keymaps.general").create_keymaps()
 require("config.keymaps.buffer").create_keymaps()
 require("config.keymaps.bufferline").create_keymaps()
