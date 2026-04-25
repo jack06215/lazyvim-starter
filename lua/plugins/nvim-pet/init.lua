159dc2231db81b80cd1605007161a43b227c88fe
Jack Cho <jack06215@gmail.com>
2026-04-25 20:55:32 +0900

    setup doge

--- lua/plugins/nvim-pet/init.lua
+++ lua/plugins/nvim-pet/init.lua
@@ -7,7 +7,25 @@ return {
       "edluffy/hologram.nvim",
     },
     config = function()
-      require("pets").setup({})
+      local pets = require("pets")
+      require("pets").setup({
+        row = 1,
+        col = 10,
+        speed_multiplier = 1,
+        default_pet = "dog",
+        default_style = "brown",
+        random = false,
+        death_animation = true,
+        popup = {
+          width = "30%",
+          winblend = 100,
+          hl = {
+            Normal = "Normal",
+          },
+          avoid_statusline = false,
+        },
+      })
+      pets.create_pet("doge", "dog", "brown")
     end,
   },
 }
